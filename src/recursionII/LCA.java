43ae3517c10568ea2118597306f39547989b0a84
Jingchi Ma <34053929+JingchiMa@users.noreply.github.com>
2018-10-01 16:15:29 -0700

    10.1 - add LCAIV

--- src/recursionII/LCA.java
+++ src/recursionII/LCA.java
@@ -1,5 +1,10 @@
 package recursionII;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 class TreeNode {
     int val;
     TreeNode left;
@@ -113,5 +118,69 @@ public class LCA {
         }
         return depth;
     }
+
+    // Question 3: LCA of K nodes, k nodes are guraunteed to be in the tree.
+
+    // Method I: solve them two by two
+
+    // 1  2  3  4  5
+    //  l1    l2   l3
+    //     ll1     ll2
+
+    // length of node list is n, # nodes in the binary tree is m
+    // time: O(mlogn)
+    // space: O(n + n/2 + ... + 1) = O(n)
+
+    public TreeNode LCAOfKNodes(TreeNode root, List<TreeNode> nodes) {
+        if (nodes.size() == 0) { // corner case
+            return null;
+        }
+        if (nodes.size() == 1) {
+            return nodes.get(0);
+        }
+        List<TreeNode> lcaNodes = new ArrayList<>();
+        for (int i = 1; i < nodes.size(); i += 2) {
+            lcaNodes.add(LCAOf2Nodes(root, nodes.get(i-1), nodes.get(i)));
+        }
+        if (nodes.size() % 2 == 1) {
+            TreeNode node = nodes.get(nodes.size() - 1);
+            lcaNodes.add(LCAOf2Nodes(root, node, node));
+        }
+        return LCAOfKNodes(root, lcaNodes);
+    }
+
+    private TreeNode LCAOf2Nodes(TreeNode root,
+                                 TreeNode node1,
+                                 TreeNode node2) {
+        if (root == null || root == node1 || root == node2) {
+            return root;
+        }
+        TreeNode left = LCAOf2Nodes(root.left, node1, node2);
+        TreeNode right = LCAOf2Nodes(root.right, node1, node2);
+        if (left != null && right != null) {
+            return root;
+        }
+        return left == null? right: left;
+    }
+
+    // Method II: 本质上就是：如果找到一个node了，那么就没有必要在搜索它的子树
+
+    public TreeNode LCAOfKNodesII(TreeNode root, List<TreeNode> nodes) {
+        Set<TreeNode> nodeSet = new HashSet<>(nodes);
+        return LCAOfKNodesHelper(root, nodeSet);
+    }
+
+    private TreeNode LCAOfKNodesHelper(TreeNode root, Set<TreeNode> nodes) {
+        if (root == null || nodes.contains(root)) {
+            return root;
+        }
+        TreeNode left = LCAOfKNodesHelper(root.left, nodes);
+        TreeNode right = LCAOfKNodesHelper(root.right, nodes);
+        if (left != null && right != null) {
+            return root;
+        }
+        return left == null ? right: left;
+    }
+
 
 }
